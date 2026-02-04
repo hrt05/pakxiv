6b40a78ef9b2de002a1fb04f617b4b0ba3b3f8a8
hrt05 <hirate05program@gmail.com>
2026-02-05 00:16:52 +0900

    nextAuthセットアップ

---
+++ src/components/topPageComponent/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@charcoal-ui/react"
+
+import '@charcoal-ui/react/dist/index.css'
+import '@charcoal-ui/react/dist/layered.css'
+
+const TopPageComponent = () => {
+    return(
+        <div>
+            <p>テスト</p>
+            <Button variant="Primary">テスト</Button>
+        </div>
+    )
+}
+
+export default TopPageComponent
