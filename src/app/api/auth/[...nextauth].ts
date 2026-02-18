63e2d73fae7e270e438453c2052c6211cfe078de
hrt05 <hirate05program@gmail.com>
2026-02-19 01:59:03 +0900

    プロバイダーの設定

--- src/app/api/auth/[...nextauth].ts
+++ src/app/api/auth/[...nextauth].ts
@@ -1,50 +1,6 @@
-import NextAuth from "next-auth"
-import { CredentialsProvider } from "next-auth/providers/credentials"
+import NextAuth from "next-auth";
+import options from '@/lib/options'
 
-// export const authOptions = {
-//     providers: [
-//         CredentialsProvider({
-//             name: "Credentials",
+const handler = NextAuth(options)
 
-//             credentials: {
-//                 username: { label: "Username", type: "text", placeholder: "jsmith" },
-//                 password: { label: "Password", type: "password" }
-//             },
-
-//             async authorize(credentials, req) {
-//                 const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
-
-//                 if (user) {
-//                     return user
-//                 } else {
-//                     return null
-//                 }
-//             }
-//         })
-//     ],
-// }
-
-export default async function auth(req: any, res: any) {
-    const providers = [
-        CredentialsProvider({
-            name: "Credentials",
-
-            credentials: {
-                username: { label: "Username", type: "text", placeholder: "jsmith" },
-                password: { label: "Password", type: "password" }
-            },
-
-            async authorize(credentials, req) {
-                const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
-
-                if (user) {
-                    return user
-                } else {
-                    return null
-                }
-            }
-        })
-    ],
-}
-
-export default NextAuth(authOptions)
+export { handler as GET, handler as POST}
---
+++ src/lib/options.ts
@@ -0,0 +1,87 @@
+import { NextAuthOptions } from "next-auth"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import Credentials from "next-auth/providers/credentials"
+import prisma from "./prisma"
+import bcrypt from "bcrypt"
+
+const options: NextAuthOptions = {
+    providers: [
+        // credentialsというプロバイダー
+        Credentials({
+
+            // 多分好きな名称でいいはず。。。。
+            id: 'credentials',
+            name: 'Credentials',
+
+            credentials: {
+                email: {
+                    label: 'Email',
+                    type: 'text',
+                },
+                password: {
+                    label: 'password',
+                    type: 'password',
+                },
+            },
+
+            // authorizeという関数に先ほどのcredentialsを入れる
+            async authorize(credentials) {
+
+                // もしcredentialsのメールとパスワードがなかったら requiredですよを返す "required"は必須という意味
+                if(!credentials?.email || !credentials.password) {
+                    throw new Error('Email and password required')
+                }
+
+                // emailが存在しているかどうかチェック
+                const user = await prisma.user.findUnique({
+                    where: { email: credentials.email }
+                })
+
+                // 存在していた場合
+                if (!user || !user.hashedpassword) {
+                    throw new Error('Email does not exists')
+                }
+
+                // bcryptで比較します
+                const  isCorrectPassword = await bcrypt.compare(
+                    credentials.password,
+                    user.hashedpassword
+                )
+
+                // もし比較して違ったら Incorrectは"正しくない"という意味
+                if (!isCorrectPassword) {
+                    throw new Error('Incorrect password')
+                }
+
+                return user
+            },
+        }),
+    ],
+
+    /** 多分ここからコールバック */
+
+    pages: {
+        signIn: '/auth'
+    },
+
+    // ここら辺のコメントは予想で書いてます
+    // NODE_ENV を developmentにする
+    debug: process.env.NODE_ENV === 'development',
+
+    // 作ったprsima変数を入れる
+    adapter: PrismaAdapter(prisma),
+
+    // セッションタイプを指定
+    session: {
+        strategy: 'jwt',
+    },
+
+    // jwtの暗号鍵の指定
+    jwt: {
+        secret: process.env.NEXTAUTH_JWT_SECRET,
+    },
+
+    // 暗号かぎの指定 part2
+    secret: process.env.NEXTAUTH_SECRET,
+}
+export default options
