695f5beb40f3926297255d19e2c7da9c8dce6f25
hrt05 <hirate05program@gmail.com>
2026-02-20 20:50:37 +0900

    ミドルウェアの設定など、足りていない型の追加、コールバックの追加（jwtとsession）

--- src/lib/options.ts
+++ src/lib/options.ts
@@ -4,6 +4,8 @@ import Credentials from "next-auth/providers/credentials"
 import prisma from "./prisma"
 import bcrypt from "bcrypt"
 
+// サインアップや、ログインのときにnextAuthがsignIn()関数を用意しており、ログインがrest API側でokになった際に実行　→　そしたらこのAPIのauthorize()が呼ばれる。
+
 const options: NextAuthOptions = {
     providers: [
         // credentialsというプロバイダー
@@ -61,6 +63,49 @@ const options: NextAuthOptions = {
 
     /** 多分ここからコールバック */
 
+    callbacks: {
+
+        // jwt()は、ログイン直後とセッションが確認されるたびに呼ばれる関数（コールバックだから）
+        async jwt({ token, user }){
+
+            // userはログイン直後だけ存在する(authorize()が返したユーザー情報) 2026/02/20 0:46 時では59行目でuserをreturnしているから
+            // 2回目以降は userがundifinedになる。
+
+            // もしログイン直後のuserがtrueならば
+            if (user) {
+                // JWTトークンにid追加して保存する
+                // user.idから取り出し、token.idに追加
+                token.id = user.id
+            }
+            return token
+        },
+
+        // session()は、useSession()やgetServerSession()が呼ばれるたびに実行される関数
+
+        // デフォルトだと、型がname, email, imagesしかないので、typesに記述します。
+        async session({ session, token }){
+            // もしsession.userがtrueならば
+            if (session.user) {
+                // tokenに保存してあったidをsessionに追加する。　上記でidが含まれているtokeをいれないといけない。
+                // これをしないとフロント側でsession.user.idが撮れない
+                session.user.id = token.id
+            }
+
+            // sessionを返すことでuseSession()の呼び出し元に渡される。 (useSession()を実行したらidの含まれているsessionが返されるような形。)
+            return session
+
+
+            // {
+            //     /** この部分はAIが提案 */
+            //     // ...session,
+            //     // user: {
+            //     //     ...session.user,
+            //     //     id: token.id
+            //     // }
+            // }
+        }
+    },
+
     pages: {
         signIn: '/auth'
     },
@@ -84,6 +129,7 @@ const options: NextAuthOptions = {
 
     // 暗号かぎの指定 part2
     secret: process.env.NEXTAUTH_SECRET,
+
 }
 export default options
 
