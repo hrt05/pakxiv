7bba02698b0a1ec35a492e07e9f681c49676982a
hrt05 <hirate05program@gmail.com>
2026-04-09 17:39:15 +0900

    自分の投稿を表示できるようにしました。

--- src/components/myPageComponent/page.tsx
+++ src/components/myPageComponent/page.tsx
@@ -1,37 +1,53 @@
 import { Session } from "next-auth"
 import styles from "./styles.module.css"
 import { headers } from "next/headers"
+import prisma from "@/lib/prisma"
 
 type ServerSessionProps = {
     serverSession: Session | null
+}
+
+type Image = {
+    id: string,
+    path: string,
+    postId: string,
 }
 
 const MyPageComponent = async ({ serverSession }: ServerSessionProps) => {
     console.log("サーバー側からのプロップス", serverSession)
     const user = await serverSession?.user
 
-    const headersData = headers();
-    const protocol = (await headersData).get('x-forwarded-proto') || 'http';
-    const host = (await headersData).get('host');
-    const apiBase = `${protocol}://${host}`
+    // const headersData = headers();
+    // const protocol = (await headersData).get('x-forwarded-proto') || 'http';
+    // const host = (await headersData).get('host');
+    // const apiBase = `${protocol}://${host}`
 
-    const userPostFetch = async () => {
-        const userPost = await fetch(`${apiBase}/api/userPost?userId=${user?.id}`, {
-            method: "GET",
-        })
+    // const userPostFetch = async () => {
+    //     const userPost = await fetch(`${apiBase}/api/userPost?userId=${user?.id}`, {
+    //         method: "GET",
+    //     })
 
-        const data = await userPost.json()
-        console.log("配列確認だよ！！！！！！！",data)
+    //     const data = await userPost.json()
+    //     console.log("配列確認だよ！！！！！！！",data)
 
-        if (userPost?.ok) {
-            console.log("ちゃんと行けたよ")
-            console.log(userPost.json)
+    //     if (userPost?.ok) {
+    //         console.log("ちゃんと行けたよ")
+    //         console.log(userPost.json)
             
-        }
+    //     }
 
-        return userPost.json
-    }
-    await userPostFetch()
+    //     return userPost.json
+    // }
+    // await userPostFetch()
+
+    const userPost = await prisma.post.findMany({
+        where: {userId: `${user?.id}`},
+        include: {
+            images: true, user: {select: {name: true}}
+        }
+    })
+
+    console.log("これどうですか？",userPost)
 
     return(
         <div>
@@ -43,7 +59,16 @@ const MyPageComponent = async ({ serverSession }: ServerSessionProps) => {
                 <p>{user?.name ?? 'omae dare'}</p>
             </div>
             <div>
-
+                <h2>自分の投稿</h2>
+                <div className={styles.postDiv}>
+                {userPost.map((item) => <div key={item.id} className={styles.post}>
+                    {/* <p>{item.id}</p> */}
+                    <p>{item.user.name}</p>
+                    <p>{item.title}</p>
+                    <p>{item.description}</p>
+                    <div>{item.images.map((image: Image, index) => <img key={index} className={styles.image} src={`https://pakxiv.s3.ap-northeast-1.amazonaws.com/${image.path}`} />)}</div>
+                </div>)}
+                </div>
             </div>
         </div>
     )
