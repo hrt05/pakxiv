43b19240838ce6292ab9c5468995a280b43e4362
hrt05 <hirate05program@gmail.com>
2026-04-24 23:57:52 +0900

    24ref

--- src/components/(myPage)/editProfileComponent/page.tsx
+++ src/components/(myPage)/editProfileComponent/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Button } from "@charcoal-ui/react"
-import { useState } from "react"
+import { useRef, useState } from "react"
 import styles from "./styles.module.css"
 
 type Props = {
@@ -13,6 +13,10 @@ const EditProfileComponent = ({imageProps}: Props) => {
 
     const [modal, setModal] = useState(false)
 
+    // refの使い方わからなくてaiに聞きました。
+    const inputRef = useRef<HTMLInputElement>(null)
+    const openInputRef = () => inputRef.current?.click()
+
     const hundleModalClick = () => {
         setModal(true)
     }
@@ -23,6 +27,7 @@ const EditProfileComponent = ({imageProps}: Props) => {
             {modal ? 
             <div role="dialog" aria-modal="true" className={styles.modal} onClick={() => setModal(false)}>
                 <div className={styles.modalIn} onClick={(e) => e.stopPropagation()}>
+                    <input type="file" ref={inputRef}/>
                 {image !== '' ? <div className={styles.iconDiv}><img className={styles.icon} src={`https://pakxiv.s3.ap-northeast-1.amazonaws.com/${image}`}/></div> : <div className={styles.iconDiv}><img className={styles.icon} src="https://pakxiv.s3.ap-northeast-1.amazonaws.com/nullIcon/%E3%81%A8%E3%81%91%E3%81%A1%E3%82%83%E3%81%86%E7%8C%AB%E3%81%95%E3%82%93.jpg"/></div>}
                 </div>
             </div> : 
