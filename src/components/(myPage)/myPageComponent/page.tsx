9dada3b238ad0be3fb9ec04948e835d6e5a5c95f
hrt05 <hirate05program@gmail.com>
2026-04-30 19:18:39 +0900

    いったんプロフィールの部分をDBから取得しました。editはまだです。

--- src/components/(myPage)/myPageComponent/page.tsx
+++ src/components/(myPage)/myPageComponent/page.tsx
@@ -1,9 +1,6 @@
 import { Session } from "next-auth"
 import styles from "./styles.module.css"
-import { headers } from "next/headers"
 import prisma from "@/lib/prisma"
-// import MyPageImageComponent from "../myPageImageComponent/page"
-import EditProfileComponent from "../editProfileComponent/page"
 
 type ServerSessionProps = {
     serverSession: Session | null
@@ -17,33 +14,25 @@ type Image = {
 
 const MyPageComponent = async ({ serverSession }: ServerSessionProps) => {
     console.log("サーバー側からのプロップス", serverSession)
-    const user = await serverSession?.user
+    const ServerSessionUser = await serverSession?.user
+    const ServerSessionUserId = ServerSessionUser?.id
 
-    // const headersData = headers();
-    // const protocol = (await headersData).get('x-forwarded-proto') || 'http';
-    // const host = (await headersData).get('host');
-    // const apiBase = `${protocol}://${host}`
+    const userData = await prisma.user.findUnique({
+        where: {
+            id: ServerSessionUserId
+        },
+        select: {
+            id: true,
+            name: true,
+            description: true,
+            image: true
+        }
+    })
 
-    // const userPostFetch = async () => {
-    //     const userPost = await fetch(`${apiBase}/api/userPost?userId=${user?.id}`, {
-    //         method: "GET",
-    //     })
-
-    //     const data = await userPost.json()
-    //     console.log("配列確認だよ！！！！！！！",data)
-
-    //     if (userPost?.ok) {
-    //         console.log("ちゃんと行けたよ")
-    //         console.log(userPost.json)
-            
-    //     }
-
-    //     return userPost.json
-    // }
-    // await userPostFetch()
+    console.log("ユーザーDB", userData)
 
     const userPost = await prisma.post.findMany({
-        where: {userId: `${user?.id}`},
+        where: {userId: ServerSessionUserId},
         include: {
             images: true, user: {select: {name: true}}
         }
@@ -55,21 +44,15 @@ const MyPageComponent = async ({ serverSession }: ServerSessionProps) => {
         <div>
             <h1>これはマイページです。</h1>
             <div>
-                <p>{user?.email}</p>
-                <p>{user?.id}</p>
-                {user?.image !== '' ? <img className={styles.icon} src={`https://pakxiv.s3.ap-northeast-1.amazonaws.com/${user?.image}`}/> : <img className={styles.icon} src="https://pakxiv.s3.ap-northeast-1.amazonaws.com/nullIcon/%E3%81%A8%E3%81%91%E3%81%A1%E3%82%83%E3%81%86%E7%8C%AB%E3%81%95%E3%82%93.jpg"/>}
-                {/* <MyPageImageComponent imageProps={user?.image}/> */}
-                <p>{user?.name ?? 'omae dare'}</p>
-            </div>
-            <div>
-                <EditProfileComponent userProps={user}/>
-                {/* <EditProfileComponent imageProps={user?.image}/> */}
+                {/* <p>{userData.email}</p> */}
+                <p>{userData?.id}</p>
+                {userData?.image !== '' ? <img className={styles.icon} src={`https://pakxiv.s3.ap-northeast-1.amazonaws.com/${userData?.image}`}/> : <img className={styles.icon} src="https://pakxiv.s3.ap-northeast-1.amazonaws.com/nullIcon/%E3%81%A8%E3%81%91%E3%81%A1%E3%82%83%E3%81%86%E7%8C%AB%E3%81%95%E3%82%93.jpg"/>}
+                <p>{userData?.name ?? 'omae dare'}</p>
             </div>
             <div>
                 <h2>自分の投稿</h2>
                 <div className={styles.postDiv}>
                 {userPost.map((item) => <div key={item.id} className={styles.post}>
-                    {/* <p>{item.id}</p> */}
                     <p>{item.user.name}</p>
                     <p>{item.title}</p>
                     <p>{item.description}</p>
---
+++ src/components/(test)/editProfileComponent/page.tsx
@@ -0,0 +1,114 @@
+'use client'
+
+import { Button, TextArea, TextField } from "@charcoal-ui/react"
+import { useRef, useState } from "react"
+import styles from "./styles.module.css"
+import { signOut, useSession } from "next-auth/react"
+// import { useSession } from "next-auth/react"
+
+// type Props = {
+//     imageProps: string | null | undefined
+// }
+// type Props = {
+//     name: string
+//     email: string
+//     image: string
+//     id: string
+// }
+
+type Props = {
+    userProps: ({
+        id?: string | null;
+        description: string;
+    } & {
+        name?: string | null;
+        email?: string | null;
+        image?: string | null;
+    }) | undefined
+}
+
+// const EditProfileComponent = ({imageProps}: Props) => {
+const EditProfileComponent = ({userProps}: Props) => {
+
+    const user = userProps
+
+    // const { update } = useSession()
+    // const clientUserId = session?.user.id
+
+    console.log(user)
+
+    const name = user?.name
+    const description = user?.description
+    const image = user?.image
+
+    const [modal, setModal] = useState(false)
+
+    /**
+     * ユーザーのuseStateなど
+     */
+    const [userName, setUserName] = useState(`${name}`)
+    const [userDescription, setUserDescription] = useState(`${description}`)
+    const [userImage, setUserImage] = useState(`${image}`)
+
+    // refの使い方わからなくてaiに聞きました。
+    const inputRef = useRef<HTMLInputElement>(null)
+    const handleClickRef = () => {
+        inputRef.current?.click();
+        console.log("アイコン変更したい。。。！！１")
+    }
+
+    const hundleModalClick = () => {
+        setModal(true)
+    }
+
+    const hundleUpdateClick = async () => {
+        console.log("あれえええええ？？？？＊",{ userName, userDescription, userImage })
+        const apiFetch = await fetch("/api/profileEdit", {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ userName, userDescription, userImage })
+        })
+
+        if (apiFetch.ok) {
+            signOut();
+            window.location.reload();
+        }
+    }
+
+    return (
+        <>
+            <Button variant="Default" onClick={hundleModalClick}>profileの編集</Button>
+            {modal ? 
+            <div role="dialog" aria-modal="true" className={styles.modal} onClick={() => setModal(false)}>
+                <div className={styles.modalIn} onClick={(e) => e.stopPropagation()}>
+                    {image !== '' ? <div className={styles.iconDiv}><img className={styles.icon} src={`https://pakxiv.s3.ap-northeast-1.amazonaws.com/${image}`}/></div> : <div className={styles.iconDiv}><img onClick={handleClickRef} className={styles.icon} src="https://pakxiv.s3.ap-northeast-1.amazonaws.com/nullIcon/%E3%81%A8%E3%81%91%E3%81%A1%E3%82%83%E3%81%86%E7%8C%AB%E3%81%95%E3%82%93.jpg"/></div>}
+                        <input hidden type="file" ref={inputRef}/>
+                    <div className={styles.name}>
+                        <p className={styles.ptagName}>名前</p>
+                        <TextField className={styles.nameField} type="text" value={userName} onChange={(value) => setUserName(value)} />
+                    </div>
+
+                    <div className={styles.descDiv}>
+                        <TextArea value={userDescription} onChange={(value) => setUserDescription(value)}></TextArea>
+                    </div>
+
+                    <div className={styles.buttons}>
+                        <div>
+                            <Button variant="Danger" className={styles.button} onClick={hundleUpdateClick}>更新</Button>
+                        </div>
+                        <div>
+                            <Button className={styles.button}>閉じる</Button>
+                        </div>
+                    </div>
+
+                </div>
+            </div> : 
+            <p>falseだよ</p>
+            }
+        </>
+    )
+}
+
+export default EditProfileComponent
